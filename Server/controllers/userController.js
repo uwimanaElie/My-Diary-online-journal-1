04b676face08ed91ae6412d221d996c01d98c61e
Elie Uwimana <umuhizielie@gmail.com>
2019-10-30 14:50:00 +0200

    feature(login):add user login functionality[finishes #169449741]

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -17,3 +17,29 @@ class Usercontroller {
 
     };
 }
+
+login = (payload) => {
+    const user = this.users.find(newUser => newUser.email === payload.email &&
+        newUser.password === payload.password);
+    if (!user) {
+        return {
+            status: 401,
+            error: 'Incorrect email or password'
+        };
+    }
+    const token = generateToken(user.email);
+    let result = {
+        UserId: user.id,
+        token: token,
+    }
+    result = {
+        status: 200,
+        message: 'User is successfully logged in',
+        data: result
+    }
+    return result;
+}
+
+isEmailTaken = email => this.users.find(u => u.email === email);
+
+export default new User();
